ad8f6e804bb47edf48324f9af6ac20ff1fc0532e
dgaudrie <gaudrie@etud.insa-toulouse.fr>
2015-10-27 16:57:31 +0100

    legeres modifications pour le post-processing (valeur de la fonction et des variables dans le fichier .stdout)

--- examples/Dakota/simple_example_opt_no_grad/myfunc.py
+++ examples/Dakota/simple_example_opt_no_grad/myfunc.py
@@ -1,5 +1,6 @@
 import sys
 import numpy
+#import dakota_utils #--> le module a creer
 
 def my_func(x):
     return x[0]**2+x[1]**2
@@ -8,12 +9,13 @@ inp_file = sys.argv[1]
 
 fid = open(inp_file,'r')
 
-#print fid.read()
+#print fid.read() #--> pour voir les details
 
 # read variables
+# x,i = dakota_utils.read_input(sys.argv[1]) #--> notre but final
 first_line = fid.readline()
 n_var = eval(first_line.split()[0])
-print "number of variables : ",n_var
+#print "number of variables : ",n_var #--> afficher le nombre de variables
 # read variables values
 x = numpy.zeros(n_var)
 varnames = []
@@ -22,9 +24,16 @@ for i in xrange(n_var):
     words = line.split()
     x[i] = eval(words[0])
     varnames.append(words[1].strip())
+while "eval_id" not in line:
+    line=fid.readline()
+line_tmp=line.split(" ")
+nit=line_tmp[-2]
+print "iteration : ",nit
 fid.close()
 
 fval = my_func(x)
+print "valeur de la fonction :",fval
+print ("arguments : "+varnames[0]+" = {}, ".format(x[0])+varnames[1]+" = {}\n".format(x[1]))
 
 # write output file
 fid = open(sys.argv[2],'w')
