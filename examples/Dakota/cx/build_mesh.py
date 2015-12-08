0e381d00d0f21a6c30d395254f5f04098fa8486e
dgaudrie <gaudrie@etud.insa-toulouse.fr>
2015-12-08 16:23:00 +0100

    modif pour utiliser des array et avoir la normale dans le bon sens

--- examples/Dakota/cx/build_mesh.py
+++ examples/Dakota/cx/build_mesh.py
@@ -17,7 +17,7 @@ def write_test_file(m,p,t,ystart,yend,nsections=5):
         fid.write('SECTION'+str(i+1)+'.T     '+str(t[i])+'\n')
     fid.close()
 
-def create_profile_linear_interp(m_root,p_root,t_root,m_tip,p_tip,t_tip,ystart,yend,chord,n_sections,n_naca_points=150):
+def create_mesh_linear_interp(m_root,p_root,t_root,m_tip,p_tip,t_tip,ystart,yend,chord,n_sections,n_naca_points=150):
     if n_sections%2 == 0:
 	raise ValueError("Merci de donner un nombre de sections impair")
     ind_root=(n_sections-1)//2
@@ -26,41 +26,68 @@ def create_profile_linear_interp(m_root,p_root,t_root,m_tip,p_tip,t_tip,ystart,y
     y_list = -semi_span*numpy.cos(theta)
     y_list[ind_root]=0. # souvent 10^-16
 
-    tip_section_begin = list(create(m_root,p_root,t_root,chord,n_naca_points))
-    root_section = list(create(m_tip,p_tip,t_tip,chord,n_naca_points))
-    tip_section_end = list(create(m_root,p_root,t_root,chord,n_naca_points))
+    Xu_tip,Xl_tip,Yu_tip,Yl_tip = create(m_tip,p_tip,t_tip,chord,n_naca_points)
+    upper_tip = numpy.array([Xu_tip,Yu_tip]).T
+    lower_tip = numpy.array([Xl_tip,Yl_tip]).T
+    tip_section = numpy.concatenate((upper_tip[::-1],lower_tip[1:]))
 
-    tip_section_begin.insert(0,-semi_span)
-    root_section.insert(0,y_list[ind_root])
-    tip_section_end.insert(0,semi_span)
+    Xu_root,Xl_root,Yu_root,Yl_root = create(m_root,p_root,t_root,chord,n_naca_points)
+    upper_root = numpy.array([Xu_root,Yu_root]).T
+    lower_root = numpy.array([Xl_root,Yl_root]).T
+    root_section = numpy.concatenate((upper_root[::-1],lower_root[1:]))
 
-    profile = [tip_section_begin]
+    # build mesh
+    vtk_model = vtk.vtkStructuredGrid()
+    vtk_model.SetDimensions(2*n_naca_points-1,n_sections,1)
+    # build points
+    vtk_points = vtk.vtkPoints()
 
-    for i in xrange(1,n_sections-1):
-        if i == ind_root:
-	    profile.append(root_section)	    
-	else:
-	    r = numpy.abs(y_list[i])/semi_span
-	    current_section = [y_list[i],(1.-r)*root_section[1] + r*tip_section_begin[1], \
-            (1.-r)*root_section[2] + r*tip_section_begin[2],(1.-r)*root_section[3] + r*tip_section_begin[3], \
-            (1.-r)*root_section[4] + r*tip_section_begin[4]]
-	    profile.append(current_section)
-    profile.append(tip_section_end)
+    for i in xrange(n_sections):
+	r = numpy.abs(y_list[i])/semi_span
+	current_section = (1.-r)*root_section + r*tip_section
+        for j in xrange(2*n_naca_points-1):
+                vtk_points.InsertNextPoint(current_section[j,0],y_list[i],current_section[j,1])
+    # set points
+    vtk_model.SetPoints(vtk_points)
+    # convert to poly data    
+    pdata_filter = vtk.vtkGeometryFilter()
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        pdata_filter.SetInput(vtk_model)
+    else:
+        pdata_filter.SetInputData(vtk_model)
+    pdata_filter.Update()
+    poly_data = pdata_filter.GetOutput()
+    
+    # compute normals
+    norms = vtk.vtkPolyDataNormals()
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        norms.SetInput(poly_data)
+    else:
+        norms.SetInputData(poly_data)
+    norms.ComputePointNormalsOff()
+    norms.ComputeCellNormalsOn()
+    norms.ConsistencyOn()
+    norms.Update()
+    
+    # clean poly data
+    clean_poly = vtk.vtkCleanPolyData()
+    clean_poly.ToleranceIsAbsoluteOn()
+    clean_poly.SetAbsoluteTolerance(1.e-6)
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        clean_poly.SetInput(norms.GetOutput())
+    else:
+        clean_poly.SetInputData(norms.GetOutput())
+    clean_poly.Update()
+    
+    # write output mesh
+    writer = vtk.vtkXMLPolyDataWriter()
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        writer.SetInput(clean_poly.GetOutput())
+    else:
+        writer.SetInputData(clean_poly.GetOutput())
+    writer.SetFileName('output.vtp')
+    writer.Write()
 
-    return profile
-
-    #ch = str(chord)
-    #fid = open('wing','w')
-    #fid.write("# id variable              valeur\n")
-    #for i in xrange(n_sections):
-     #   fid.write('SECTION'+str(i+1)+'.Y     '+str(y_list[i])+'\n')
-      #  fid.write('SECTION'+str(i+1)+'.CORDE '+ch+'\n')
-       # fid.write('SECTION'+str(i+1)+'.M     '+str(m[i])+'\n')
-        #fid.write('SECTION'+str(i+1)+'.P     '+str(p[i])+'\n')
-        #fid.write('SECTION'+str(i+1)+'.T     '+str(t[i])+'\n')
-   # fid.close()
-
-    return profile
 
 def create_mesh(M,P,T):
 	n_naca_pts = 50
@@ -123,63 +150,4 @@ def create_mesh(M,P,T):
 	writer.SetFileName('output.vtp')
 	writer.Write()
 
-def create_mesh_linear_interp(m_root,p_root,t_root,m_tip,p_tip,t_tip,ystart,yend,chord,n_sections):
-	n_naca_pts = 50
-	wing=create_profile_linear_interp(m_root,p_root,t_root,m_tip,p_tip,t_tip,ystart,yend,chord,n_sections,n_naca_pts)
-	n_section_pts = 2*n_naca_pts-1
-
-	# build mesh
-	vtk_model = vtk.vtkStructuredGrid()
-	vtk_model.SetDimensions(n_section_pts,n_sections,1)
-	# build points
-	vtk_points = vtk.vtkPoints()
-
-	for j in xrange(n_sections):
-	    upper_pts = numpy.array([wing[j][1],wing[j][3]]).T
-	    lower_pts = numpy.array([wing[j][2],wing[j][4]]).T
-	    section_pts = numpy.concatenate((lower_pts[::-1],upper_pts[1:]))
-	    for i in xrange(n_section_pts):
-		vtk_points.InsertNextPoint(section_pts[i,0],wing[j][0],section_pts[i,1])
-	# set points
-	vtk_model.SetPoints(vtk_points)
-
-	# convert to poly data    
-	pdata_filter = vtk.vtkGeometryFilter()
-	if vtk.VTK_MAJOR_VERSION <= 5:
-	    pdata_filter.SetInput(vtk_model)
-	else:
-	    pdata_filter.SetInputData(vtk_model)
-	pdata_filter.Update()
-	poly_data = pdata_filter.GetOutput()
-
-	# compute normals
-	norms = vtk.vtkPolyDataNormals()
-	if vtk.VTK_MAJOR_VERSION <= 5:
-	    norms.SetInput(poly_data)
-	else:
-	    norms.SetInputData(poly_data)
-	norms.ComputePointNormalsOff()
-	norms.ComputeCellNormalsOn()
-	norms.ConsistencyOn()
-	norms.Update()
-
-	# clean poly data
-	clean_poly = vtk.vtkCleanPolyData()
-	clean_poly.ToleranceIsAbsoluteOn()
-	clean_poly.SetAbsoluteTolerance(1.e-6)
-	if vtk.VTK_MAJOR_VERSION <= 5:
-	    clean_poly.SetInput(norms.GetOutput())
-	else:
-	    clean_poly.SetInputData(norms.GetOutput())
-	clean_poly.Update()
-
-	# write output mesh
-	writer = vtk.vtkXMLPolyDataWriter()
-	if vtk.VTK_MAJOR_VERSION <= 5:
-	    writer.SetInput(clean_poly.GetOutput())
-	else:
-	    writer.SetInputData(clean_poly.GetOutput())
-	writer.SetFileName('output.vtp')
-	writer.Write()
-
-create_mesh_linear_interp(0.,0.4,0.12,0.01,0.38,0.10,-5.,5.,1.,15)
+create_mesh_linear_interp(0.,0.12,0.4,0.1,0.15,0.3,-2.5,2.5,1.,21,50)
