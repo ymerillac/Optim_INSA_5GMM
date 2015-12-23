c85d9b931ff5df77d1d1a88136fda022a3740994
dgaudrie <gaudrie@etud.insa-toulouse.fr>
2015-12-23 23:53:39 +0100

    optimisation genetique

--- examples/Dakota/min_cx/myfunc.py
+++ examples/Dakota/min_cx/myfunc.py
@@ -9,12 +9,14 @@ from apame_utils import run_case
 n_sections=31
 n_naca_pts=50
 chord=[1.,1.]
-span=10.
+Sref=10.
+Re=5.e5
 
-def my_func(M,P,T):
+def my_func(M,P,T,aoa,span,tip_chord):
+        chord[1] = tip_chord
 	mesh_utils.write_wing_file(M,P,T,chord,span,n_sections,n_naca_pts)
 	NACA.create_wing('current_wing','output')
-	run_apame()
+	run_apame(aoa)
 	fid = open('output_polar.dat','r')
 	nb_lines=0
 	while fid.readline():
@@ -35,10 +37,13 @@ def my_func(M,P,T):
 		cz[i]=float(tmp[1])
 	fid.close()
 	ind = cx.argmin()
-	return min(cx),cz[ind]
+	Cf = 0.0583/Re**0.2
+	Sw = (chord[0]+chord[1])*span/2.
+        Cd_friction = Sw/Sref*Cf
+	return min(cx)+Cd_friction,cz[ind]
 
 
-def run_apame():
+def run_apame(aoa):
 	# compute flight conditions
 	r=287.058 #J.kg-1.K-1
 	Patm = 101325.#Pa
@@ -48,13 +53,12 @@ def run_apame():
 	Tc = T-273.15
 	print "T=",T
 
-	Re=5.e5
 	airspeed = Re*mu/rho
 
 	print "Airspeed=",airspeed*3.6
 
 	# list of alpha and beta to compute
-	alpha_list = numpy.array([3])
+	alpha_list = numpy.array([aoa])
 	beta_list = numpy.zeros(1)
 
 	run_case('output.vtp',
@@ -68,16 +72,19 @@ def run_apame():
 		 origin=[0.,0.,0.],
 		 wingspan=10.,
 		 ref_chord=1.,
-		 Sref=10.,
+		 Sref=Sref,
 		 method=0,
 		 farfield_dist=50.,
 		 velorder=1)
 
 X,varnames=dakota_utils.read_input(sys.argv[1])
-M=[X[0],X[1]]
-P=[X[2],X[3]]
-T=[X[4],X[5]]
+M = [X[0],X[1]]
+P = [X[2],X[3]]
+T = [X[4],X[5]]
+aoa = X[6]
+span = X[7]
+tip_chord = X[8]
 
-fval,cstr = my_func(M,P,T)
+fval,cstr = my_func(M,P,T,aoa,span,tip_chord)
 dakota_utils.write_outputs(sys.argv[2],[fval,-cstr+0.5],['my_function','my_constraint'])
     
