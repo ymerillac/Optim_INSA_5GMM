c077b9b7b56d7ea7fc835fe55aefef9e39eb09c3
dgaudrie <gaudrie@etud.insa-toulouse.fr>
2016-01-13 01:12:02 +0100

    quelques plots et données

--- modules/build_initial_wing.py
+++ modules/build_initial_wing.py
@@ -12,7 +12,7 @@ span=10.
 Re=5.e5
 Sref=10.
 
-def run_apame(nb_angles):
+def run_apame(aoa):
 	# compute flight conditions
 	r=287.058 #J.kg-1.K-1
 	Patm = 101325.#Pa
@@ -27,9 +27,8 @@ def run_apame(nb_angles):
 	print "Airspeed=",airspeed*3.6
 
 	# list of alpha and beta to compute
-	alpha_span=float((nb_angles-1)//2)
-	alpha_list = numpy.linspace(-alpha_span,alpha_span,nb_angles)
-	beta_list = numpy.zeros(nb_angles)
+	alpha_list = numpy.array([aoa])
+	beta_list = numpy.zeros(1)
 
 	run_case('initial'+str(nb)+'.vtp',
 		 wake_length=10.,
@@ -48,19 +47,19 @@ def run_apame(nb_angles):
 		 velorder=1)
 
 if nb==8415:
-	nb_angles=11
+	aoa=-3.15
 	M=[0.08,0.08]
 	P=[0.4,0.4]
 	T=[0.15,0.15]
 	
 elif nb==12:
-	nb_angles=21
+	aoa=5.87
 	M=[0.0,0.0]
 	P=[0.4,0.4]
 	T=[0.12,0.12]
 
 mesh_utils.write_wing_file(M,P,T,chord,span,n_sections,n_naca_pts)
 NACA.create_wing('current_wing','initial'+str(nb))
-run_apame(nb_angles)
+run_apame(aoa)
 
 
